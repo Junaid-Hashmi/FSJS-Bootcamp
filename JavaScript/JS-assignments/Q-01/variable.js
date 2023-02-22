c819f87ca9ef72e0cf8093795c9765b8e3b17992
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-22 22:00:22 +0530

    initial commit

---
+++ JavaScript/JS-assignments/Q-01/variable.js
@@ -0,0 +1,11 @@
+let name = "junaid";
+let adult = true;
+let age = 21;
+let college;
+let a = null;
+
+console.log(`Type of '${name}' - ${typeof name} `);
+console.log(`Type of '${adult}' - ${typeof adult} `);
+console.log(`Type of '${age}' - ${typeof age} `);
+console.log(`Type of '${college}' - ${typeof college} `);
+console.log(`Type of '${a}' - ${typeof a} `);
