8c465cf51a2bf613273d69ceb746a77dd7fc8cc4
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-22 22:37:26 +0530

    q6

---
+++ JavaScript-assignments/Q-06/split2.js
@@ -0,0 +1,14 @@
+let myStr = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+console.log(myStr.split(","));
+
+// output
+// [
+//   'Facebook',
+//   ' Google',
+//   ' Microsoft',
+//   ' Apple',
+//   ' IBM',
+//   ' Oracle',
+//   ' Amazon'
+// ]
