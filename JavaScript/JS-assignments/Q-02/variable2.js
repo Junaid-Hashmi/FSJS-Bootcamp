cf2291a838f8496788afb6d2e451a32acddb8c62
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-22 22:12:38 +0530

    q2 added

---
+++ JavaScript/JS-assignments/Q-02/variable2.js
@@ -0,0 +1,10 @@
+let firstName = "Junaid";
+let lastName = "Hashmi";
+let maritalStatus = false;
+let country = "India";
+let age = 21;
+
+console.log(`Hi! I am ${firstName} ${lastName}. I am ${age} years old.`);
+console.log(
+  `I am from ${country} and I am ${maritalStatus ? "married" : "a bachelor"}.`
+);
