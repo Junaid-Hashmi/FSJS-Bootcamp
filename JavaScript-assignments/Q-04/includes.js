08359f2d9e9a16924c41cd831c209ca5235e0c3c
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-22 22:25:56 +0530

    q4 done

---
+++ JavaScript-assignments/Q-04/includes.js
@@ -0,0 +1,8 @@
+let myStr =
+  "In a few thousand years of humans developing their civilizations, there always has been a big question and that is how human mind actually works? ";
+
+console.log(myStr.includes("humans"));
+// output -> true
+
+console.log(myStr.includes("robots"));
+// output -> false
