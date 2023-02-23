bcda29830cdae4738fd034e999795ee9f3ed780d
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-23 22:25:06 +0530

    q7

---
+++ JavaScript-assignments/Q-07/lastIndex.js
@@ -0,0 +1,15 @@
+let arr = ["udemy", "khan academy", "pw skills", "udacity", "coursera"];
+let arr2 = [
+  "udemy",
+  "khan academy",
+  "pw skills",
+  "udacity",
+  "pw skills",
+  "coursera",
+];
+console.log(arr.lastIndexOf("pw skills"));
+console.log(arr2.lastIndexOf("pw skills"));
+
+// Output
+// 2
+// 4
