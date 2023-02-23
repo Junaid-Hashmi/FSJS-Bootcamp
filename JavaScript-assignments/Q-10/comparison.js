8c43cc464e423a0a4baae5aaa139884a8e07a232
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-23 22:51:38 +0530

    q10

---
+++ JavaScript-assignments/Q-10/comparison.js
@@ -0,0 +1,27 @@
+console.log(4 > 3);
+// true
+console.log(4 >= 3);
+// true
+console.log(4 < 3);
+// false
+console.log(4 <= 3);
+// false
+console.log(4 == 4);
+// true
+console.log(4 === 4);
+// true
+console.log(4 != 4);
+// false
+console.log(4 !== 4);
+// false
+console.log(4 != "4");
+// false
+console.log(4 == "4");
+// true
+console.log(4 === "4");
+// false
+
+console.log(`Length of python: ${"python".length}`);
+console.log(`Length of jargon: ${"jargon".length}`);
+console.log(`${"python".length > "jargon".length}`);
+// false
