07472690b5b6ac819d6104ad82754d60dabb930a
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-23 22:41:02 +0530

    q9

---
+++ JavaScript-assignments/Q-09/boolean.js
@@ -0,0 +1,10 @@
+console.log("sjh" == "sjh");
+console.log("111" == 111);
+console.log(1 <= 1);
+// true
+
+console.log(2 * 6 >= 10 + 3);
+console.log("111" === 111);
+console.log(123 !== 123);
+console.log("true" == true);
+// false
