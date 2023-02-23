d6964d945428b664c0fd29e4a7dfbae9a2de4ab9
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-23 22:34:08 +0530

    q8

---
+++ JavaScript-assignments/Q-08/trim.js
@@ -0,0 +1,8 @@
+let myStr = "   sjh   ";
+let trimmedMyStr = myStr.trim();
+console.log(`Before trim: ${myStr}, ${myStr.length}`);
+console.log(`After trim: ${trimmedMyStr}, ${trimmedMyStr.length}`);
+
+// Output
+// Before trim:    sjh   , 9
+// After trim: sjh, 3
