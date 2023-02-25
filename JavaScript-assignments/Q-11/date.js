22b092931a0daf2565153eba7ed43efdc7f3f9f5
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-02-25 14:31:51 +0530

    q11

---
+++ JavaScript-assignments/Q-11/date.js
@@ -0,0 +1,17 @@
+let now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(Date.now());
+
+// Output
+// 2023
+// 1
+// 25
+// 6
+// 14
+// 30
+// 1677315650197
