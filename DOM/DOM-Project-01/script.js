1f77810c870463d2b7e465a16befae53c35052e2
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-03-08 19:48:23 +0530

    task2

--- DOM/DOM-Project-01/script.js
+++ DOM/DOM-Project-01/script.js
@@ -1,6 +1,6 @@
 // --------------------Task 01------------------
 // adding an li element to nav ul
-let navList = document.querySelector("nav ul");
+const navList = document.querySelector("nav ul");
 let newLiElement = document.createElement("li");
 newLiElement.innerHTML = `<a href="#">Hire Me</a>`;
 navList.appendChild(newLiElement);
@@ -8,3 +8,12 @@ navList.appendChild(newLiElement);
 // hiding all social media icons
 let footerIcons = document.querySelector("footer ul");
 footerIcons.style.visibility = "hidden";
+
+// ------------------Task 02------------------------
+// hiding 'hire me' nav link
+const navLiAll = navList.children;
+navLiAll[navLiAll.length - 1].style.display = "none";
+
+// changing search bar's placeholder text
+const searchFieldInput = document.querySelector(".search-field input");
+searchFieldInput.placeholder = "Search My Project";
