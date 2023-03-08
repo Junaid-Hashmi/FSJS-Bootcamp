285a1cbcacdb3f027078e0c7955ef74c6afe66fc
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-03-08 20:11:25 +0530

    task4

--- DOM/DOM-Project-01/script.js
+++ DOM/DOM-Project-01/script.js
@@ -1,4 +1,4 @@
-// --------------------Task 01------------------
+// ----------------------------------- Task 01 ---------------------------------------------
 // adding an li element to nav ul
 const navList = document.querySelector("nav ul");
 let newLiElement = document.createElement("li");
@@ -9,7 +9,7 @@ navList.appendChild(newLiElement);
 let footerIcons = document.querySelector("footer ul");
 footerIcons.style.visibility = "hidden";
 
-// ------------------Task 02------------------------
+// --------------------------------- Task 02 ------------------------------------------------
 // hiding 'hire me' nav link
 const navLiAll = navList.children;
 navLiAll[navLiAll.length - 1].style.display = "none";
@@ -17,3 +17,15 @@ navLiAll[navLiAll.length - 1].style.display = "none";
 // changing search bar's placeholder text
 const searchFieldInput = document.querySelector(".search-field input");
 searchFieldInput.placeholder = "Search My Project";
+
+// --------------------------------- Task 03 ------------------------------------------------
+// changing the placeholder text again to it's previous text
+searchFieldInput.placeholder = "Search";
+
+//bringing back the footer social media icons
+footerIcons.style.visibility = "visible";
+
+// --------------------------------- Task 04 ------------------------------------------------
+// changing the image
+const imageEl = document.querySelector(".hero-right-section img");
+imageEl.src = "./myImage.jpeg";
