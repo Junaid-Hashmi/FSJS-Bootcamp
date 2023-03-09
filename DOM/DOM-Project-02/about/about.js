98d43400dbf749a2bf2053e669e4ab9a9cd091a6
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2023-03-09 21:47:45 +0530

    dom project2

--- DOM/DOM-Project-02/about/about.js
+++ DOM/DOM-Project-02/about/about.js
@@ -1,3 +1,29 @@
+// -------------------------------Task 2-------------------------------------
+// adding a new accordion section 'skills'
+const accordionWrapper = document.querySelector(".accordian-wrapper");
+let newAccordion = document.createElement("div");
+newAccordion.classList.add("accordian");
+accordionWrapper.appendChild(newAccordion);
+
+let newh3 = document.createElement("h3");
+let newText = document.createElement("p");
+
+newh3.textContent = "Skills";
+newText.textContent =
+  "I possess a good command over the Full Stack Development technologies like MERN which can be seen in my work over Github.";
+
+newAccordion.appendChild(newh3);
+newAccordion.appendChild(newText);
+console.log(accordionWrapper.innerHTML);
+
+// -------------------------------Task 1-------------------------------------
+// changing bgc of all accordion headings
+let h3All = document.querySelectorAll("h3");
+for (let heading of h3All) {
+  heading.style.backgroundColor = "#dadaf8";
+}
+
+// ------------------------------------------------------------------------
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
